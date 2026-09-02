eb8d15c925a98a6425eeefabee62e3ecb0c0aab6
REnata-Stan <reyhanehalipour5@gmail.com>
2026-09-02 09:20:39 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTop(src) {
+  document.getElementById("top").src = src;
+}
+
+function changeBottom(src) {
+  document.getElementById("bottom").src = src;
+}
+
+function resetClothes() {
+  document.getElementById("top").src = "";
+  document.getElementById("bottom").src = "";
+}
